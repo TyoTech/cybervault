4200845db6ae9d7253c580d953b96e630cda18c0
TyoSaja <budisulistiyokds@.com>
2026-07-06 11:10:03 +0700

    fix: konfigurasi vite dan script build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import taildwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        taildwindcss(),
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
